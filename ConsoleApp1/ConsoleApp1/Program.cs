f4b7d2a09ab2652fd6321e6fb7b8fde631ec4fe7
Hugo Mata <Hugo.Mata@snclavalin.com>
2020-10-07 10:27:06 -0300

    Mudança no WriteLine

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Primeira mudança.");
             Console.ReadKey();
         }
     }
